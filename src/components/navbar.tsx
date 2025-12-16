a498b6d427a90e2c930e77aee779f3610591799f
WEB5D <info@blkluv.org>
2025-12-16 15:53:06 -0500

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -75,7 +75,7 @@ export function Navbar() {
 
   return (
     <div className='flex justify-between items-center mb-6'>
-      <h1 className='text-2xl font-bold'>Simple Prediction Market</h1>
+      <h1 className='text-2xl font-bold'>BASE.YENO.BET</h1>
       <div className='items-center flex gap-2'>
         {account && (
           <>
